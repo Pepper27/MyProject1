b21f5774fcd3892b553e9545febffe3213ae8894
macbook pro <macbookpro@MacBook-Pro-cua-macbook-2.local>
2024-12-03 21:44:18 +0700

    Im nice

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                colorMain:"black",
+                colorSub:"#F0F0F0"
+            },
+            fontFamily: {
+                fontBe:"Be Vietnam Pro"
+            }
+        },
+        screens: {
+            "sm":'576px',
+            "md":'768px',
+            "lg":'992px',
+            "xl":'1240px',
+            "2xl":'1440px'
+        }
+    }
+  }
